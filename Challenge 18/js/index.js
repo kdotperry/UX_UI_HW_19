f718ec8dfcc83b51ab5ec3bfdf736c312f13371a
kdotperry <167252722+kdotperry@users.noreply.github.com>
2024-05-20 19:28:54 -0400

    UDATES

--- Challenge 18/js/index.js
+++ Challenge 18/js/index.js
@@ -1,6 +1,27 @@
 console.log('Your index file has loaded correctly');
-    $(#myButton).ready(function(){
-      $("#downloadResumeButton").click(function(){
-        window.location.href = "Images/KPERRY_UI_Resume.pdf";
-      });
-    });
+
+var buttons = document.querySelectorAll('.myButton'); 
+console.log(buttons);
+
+
+document.getElementById("#myButton").addEventListener('click', function() {
+
+
+//   buttons = window.location.href;
+  console.log(buttons);
+         
+//   var pdfPath = this.getAttribute('Images/mywork.pdf');
+//   console.log(pdfPath)
+  // Redirect to the specified PDF
+  location.replace("./Images/myworkpdf");
+});
+
+// Add a click event listener to each button
+// buttons.forEach(function(button) {
+//     button.addEventListener('click', function() {
+//         // Get the value of the data-pdf attribute
+//         var pdfPath = this.getAttribute('data-pdf');
+//         // Redirect to the specified PDF
+//         window.location.href = pdfPath;
+//     });
+// });
